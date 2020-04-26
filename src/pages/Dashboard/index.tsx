4571ce6eb8198ee82d63f2e606374473623a206f
Mardonio Costa <mardonio@live.com>
2020-04-26 13:06:06 -0300

    Estilizando dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
 
-import { Title } from './style';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoimg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './style';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explorer repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoimg} alt="Github Explore" />
+      <Title>Explorer repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/35425708?s=460&u=fe2a2a2f7e53f4a77cab22c39f44831cb1857d31&v=4"
+            alt="Mardonio Costa"
+          />
+          <div>
+            <strong>Mardoniosc/recursos</strong>
+            <p>guardar recursos adicionais para programação</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/35425708?s=460&u=fe2a2a2f7e53f4a77cab22c39f44831cb1857d31&v=4"
+            alt="Mardonio Costa"
+          />
+          <div>
+            <strong>Mardoniosc/recursos</strong>
+            <p>guardar recursos adicionais para programação</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/35425708?s=460&u=fe2a2a2f7e53f4a77cab22c39f44831cb1857d31&v=4"
+            alt="Mardonio Costa"
+          />
+          <div>
+            <strong>Mardoniosc/recursos</strong>
+            <p>guardar recursos adicionais para programação</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
